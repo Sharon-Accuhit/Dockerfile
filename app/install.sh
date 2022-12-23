e24e665866071ecbde3c0019b0d2960d4405ffb3
Sharon-Accuhit <sharon.chen@accuhit.net>
2022-12-23 11:39:46 +0800

    01

---
+++ app/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#install php
+apt-get update && apt-get upgrade -y
+echo "install php ok"
+
+apt-get install busybox -y
---
+++ app/set.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#copy config to folder
+cp config/* /tmp
+
+mkdir -p /opt/www && echo '"Accuhit IT"' > /opt/www/index.html
