ee7a21c0af985ce04e8c92c1e68752890f25c38e
Sharon-Accuhit <sharon.chen@accuhit.net>
2023-01-09 11:42:29 +0800

    33

--- codedeploy/container.sh
+++ codedeploy/container.sh
@@ -13,11 +13,11 @@ systemctl start docker
 source config.txt
 
 #pull image
-#echo Logging in to Amazon ECR...
-#aws ecr get-login-password --region $AWS_DEFAULT_REGION | \
-#docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
-#echo Pulling the Docker image...
-#docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
+echo Logging in to Amazon ECR...
+aws ecr get-login-password --region $AWS_DEFAULT_REGION | \
+docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
+echo Pulling the Docker image...
+docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
 
 #run container
 docker run --name=container -h test --restart=always -itd \
