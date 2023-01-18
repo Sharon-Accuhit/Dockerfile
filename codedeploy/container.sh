fe930efac3515d6347b702d860e97c9c5664dc62
Sharon-Accuhit <sharon.chen@accuhit.net>
2023-01-18 11:31:45 +0800

    47

--- codedeploy/container.sh
+++ codedeploy/container.sh
@@ -13,18 +13,18 @@ systemctl start docker
 #set variable
 source config.txt
 
-#pull image
-#echo Logging in to Amazon ECR...
-#aws ecr get-login-password --region $AWS_DEFAULT_REGION | \
-#docker login --username AWS --password-stdin \
-#$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
-#echo Pulling the Docker image...
-#docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
-
 #remove the past image
 docker images | grep "$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME" 
 &>/dev/null
 [ $? = 0 ] && docker rmi -f $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME
+
+#pull image
+echo Logging in to Amazon ECR...
+aws ecr get-login-password --region $AWS_DEFAULT_REGION | \
+docker login --username AWS --password-stdin \
+$AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
+echo Pulling the Docker image...
+docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
 
 #run container
 docker ps -a | grep container &>/dev/null
